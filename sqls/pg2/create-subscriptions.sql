bd221f311d4c437b8e946eda50ea0a069e073636
Stéphane Klein <contact@stephane-klein.info>
2020-04-26 17:41:56 +0200

    Add user param missing in create subcription command

--- sqls/pg2/create-subscriptions.sql
+++ sqls/pg2/create-subscriptions.sql
@@ -1,3 +1,3 @@
 CREATE SUBSCRIPTION my_sub
-    CONNECTION 'host=postgres1 port=5432 dbname=postgres password=password'
+    CONNECTION 'host=postgres1 port=5432 dbname=postgres user=postgres password=password'
     PUBLICATION my_pub;
